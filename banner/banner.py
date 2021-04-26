711e061a745b19d4ce6e9a6f6a07fd33e4b1fdc5
BLACK LEGION <82705654+blvckteam@users.noreply.github.com>
2021-04-26 14:32:43 +0300

    Update banner.py

--- banner/banner.py
+++ banner/banner.py
@@ -9,7 +9,7 @@ class Banner(object):
             print(f'{F.RED} / /_/ / /___/ ___ / /___/ /| |{F.WHITE}/ /|  / /___  / /    ')
             print(f'{F.RED}/_____/_____/_/  |_\____/_/ |_/{F.WHITE}_/ |_/_____/ /_/  {B.RED}1.2{B.RESET}')
             print('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
-            print(f'OS: {F.RED}{sys.platform}{F.WHITE}                         by {F.RED}@theblacklegion{F.WHITE}')
+            print(f'OS: {F.RED}{sys.platform}{F.WHITE}                         by {F.RED}@blvckteam{F.WHITE}')
             print('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
             print('')
 
